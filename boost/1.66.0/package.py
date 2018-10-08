e3c23be3ba82d998155a2aa7489d8ccde45f96ea
Jason Gilholme <jasongilholme@gmail.com>
2018-10-08 18:52:52 +1100

    Add boost & vfx reference platform

---
+++ boost/1.66.0/package.py
@@ -0,0 +1,28 @@
+name = "boost"
+
+version = "1.66.0"
+
+authors = [
+    "boost.org"
+]
+
+description = \
+    """
+    Peer-reviewed portable C++ source libraries.
+    """
+
+build_requires = []
+
+@early()
+def variants():
+    from rez.package_py_utils import expand_requires
+    requires = ["platform-**", "arch-**", "os-**"]
+    return [expand_requires(*requires)]
+
+uuid = "repository.boost"
+
+def commands():
+    if building:
+        # cmake FindPackage env vars
+        env.BOOST_ROOT = "{root}"
+        env.BOOST_INCLUDEDIR = "{root}/include"
---
+++ vfx_reference_platform/2018/package.py
@@ -0,0 +1,32 @@
+name = "vfx_reference_platform"
+
+version = "2015"
+
+authors = [
+    "www.vfxplatform.com"
+]
+
+description = \
+    """
+    Bundle for VFX Reference Platform.
+    """
+
+requires = [
+    "~python-2.7.5+<2.8",
+    "~qt-5.6.1+<5.7",
+    "~pyqt-5.6",
+    "~pyside-2.0,
+    "~numpy-1.12.1",
+    "~openexr-2.2",
+    "~ptex-2.1.28",
+    "~opensubdiv-3.3",
+    "~openvdb-5",
+    "~alembic-1.7",
+    "~opencolorio-1.0.9",
+    "~aces-1.0.3+<1.1",
+    "~boost-1.61",
+    "~tbb-2017.2",
+    "~mkl-0.12"
+]
+
+uuid = "repository.vfx_reference_platform"
